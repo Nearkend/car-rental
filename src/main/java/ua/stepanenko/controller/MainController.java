3819b3dfed20f0d0d86985bf36f3559b1fc6b9b3
Eugen Stepanenko <nearkend@gmail.com>
2021-03-01 16:02:15 +0200

    Alex please

---
+++ src/main/java/ua/stepanenko/controller/MainController.java
@@ -0,0 +1,13 @@
+package ua.stepanenko.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/hello-world")
+    public String sayHello(){
+        return "hello_world";
+    }
+}
